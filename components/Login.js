7dec0a0352c097457c2ace08a615c2e48219abc5
your name <abukarakiomar@gmail.com>
2024-08-12 10:54:38 +0300

    first commit

---
+++ components/Login.js
@@ -0,0 +1,13 @@
+import { FaFacebook } from "react-icons/fa";
+
+const Login = () => {
+  return (
+    <div className='grid grid-cols-2'>
+        <div className='bg-blue-500 h-screen grid place-items-center'>
+        <FaFacebook className="text-6xl text-white"/>
+        </div>
+    </div>
+  )
+}
+
+export default Login
