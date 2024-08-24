92dd290a41fac7254fa2ba98a88aedaf99f2c8aa
your name <abukarakiomar@gmail.com>
2024-08-24 11:47:11 +0300

    change secret key

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -112,8 +112,9 @@ export const authOptions = {
       return session;
     },
   },
-  secret:process.env.NEXTAUTH_SECRET,
+  secret:process.env.SECRET,
 };
+
 
 export default NextAuth(authOptions);
 
